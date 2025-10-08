12482534a908b7744f853a51ee6a33175824ec71
ArthurHDEV <arthurhap.pro@gmail.com>
2025-10-08 15:38:05 -0300

    Initial commit

---
+++ src/main/java/br/com/ifpb/hibernate_restaurant_order_system/HibernateRestaurantOrderSystemApplication.java
@@ -0,0 +1,13 @@
+package br.com.ifpb.hibernate_restaurant_order_system;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HibernateRestaurantOrderSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HibernateRestaurantOrderSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/ifpb/hibernate_restaurant_order_system/HibernateRestaurantOrderSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.ifpb.hibernate_restaurant_order_system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HibernateRestaurantOrderSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

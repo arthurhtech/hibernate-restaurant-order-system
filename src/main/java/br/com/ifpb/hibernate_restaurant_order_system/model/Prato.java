d37ef4b9ce8a40102f4fd2d231badab1d22fdb47
Guilherme <guilherme.torres@academico.ifpb.edu.br>
2025-10-16 14:11:57 -0300

    CDPR-35 Criação da classe Prato

---
+++ src/main/java/br/com/ifpb/hibernate_restaurant_order_system/model/Prato.java
@@ -0,0 +1,29 @@
+package br.com.ifpb.hibernate_restaurant_order_system.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+public class Prato {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long id;
+
+    @Column(nullable = false, length = 50)
+    private String nome;
+
+    @Column(nullable = false, length = 200)
+    private String descricao;
+
+    @Column(nullable = false)
+    private double preco;
+
+    public Prato(long id, String nome, String descricao, double preco) {
+        this.id = id;
+        this.nome = nome;
+        this.descricao = descricao;
+        this.preco = preco;
+    }
+
+}

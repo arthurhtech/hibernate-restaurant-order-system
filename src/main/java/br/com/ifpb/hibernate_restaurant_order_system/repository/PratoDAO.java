12fdecae3c2c8b2d958ea2c3277f39bce63a0ddc
Guilherme <guilherme.torres@academico.ifpb.edu.br>
2025-10-16 15:14:28 -0300

    CDPR-37 Criação da Classe PratoDAO

---
+++ src/main/java/br/com/ifpb/hibernate_restaurant_order_system/repository/PratoDAO.java
@@ -0,0 +1,36 @@
+package br.com.ifpb.hibernate_restaurant_order_system.repository;
+
+import br.com.ifpb.hibernate_restaurant_order_system.model.Prato;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityManagerFactory;
+
+import java.util.List;
+
+public class PratoDAO {
+    private final EntityManagerFactory emf;
+
+    public PratoDAO(EntityManagerFactory emf) {
+        this.emf = emf;
+    }
+    public Prato save (Prato prato){
+        try(EntityManager em = emf.createEntityManager()){
+            try {
+                em.getTransaction().begin();
+                em.persist(prato);
+                em.getTransaction().commit();
+            } catch (Exception e) {
+                if (em.getTransaction().isActive()){
+                    em.getTransaction().rollback();
+                }
+                throw new RuntimeException("Erro ao salvar o prato: " + e.getMessage(), e);
+            }
+            return prato;
+        }
+    }
+    public List<Prato> findAll() {
+        try(EntityManager em = emf.createEntityManager()){
+            String jpql = "select p from Prato p";
+            return em.createQuery(jpql, Prato.class).getResultList();
+        }
+    }
+}

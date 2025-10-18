f0e3de8c813641d5bc9924a9e4fd9cffdcc27aab
brunnofdev <brunnofdev@gmail.com>
2025-10-18 14:31:15 -0300

    GDPR-42 - Endpoind /Post no PratoCOntroller

--- src/main/java/br/com/ifpb/hibernate_restaurant_order_system/controller/PratoController.java
+++ src/main/java/br/com/ifpb/hibernate_restaurant_order_system/controller/PratoController.java
@@ -1,7 +1,12 @@
 package br.com.ifpb.hibernate_restaurant_order_system.controller;
 
+import br.com.ifpb.hibernate_restaurant_order_system.dto.prato.PratoRequestDTO;
+import br.com.ifpb.hibernate_restaurant_order_system.dto.prato.PratoResponseDTO;
 import br.com.ifpb.hibernate_restaurant_order_system.service.PratoService;
 import jakarta.persistence.EntityManagerFactory;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -15,5 +20,9 @@ public class PratoController {
         this.pratoService = new PratoService(emf);
     }
 
-
+    @PostMapping
+    public ResponseEntity<PratoResponseDTO> save(@RequestBody PratoRequestDTO pratoRequestDTO) {
+        PratoResponseDTO pratoResponseDTO = pratoService.save(pratoRequestDTO);
+        return ResponseEntity.status(201).body(pratoResponseDTO);
+    }
 }
